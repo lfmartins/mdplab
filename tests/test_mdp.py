0a60e78099c04450136af31ec0d85e2da838d600
L. Felipe Martins <luizfelipe.martins@gmail.com>
2026-02-20 13:27:34 -0500

    Fix recycling robot test

--- tests/test_mdp.py
+++ tests/test_mdp.py
@@ -11,8 +11,10 @@ def test_public_api_import():
 
 recycling_robot_cases = [
     (0.8, 0.6, 15, 10, -3),
+    (0.9, 0.3, 20, 12, -5),
+    (0.4, 0.5, 12,  8, -1)
 ]
-def test_recycling_robot_creation(recycling_robot_mdp: MDP):
+def test_recycling_robot_creation():
     states = ["high", "low"]
     actions = ["search", "wait", "recharge"]
 
@@ -21,36 +23,36 @@ def test_recycling_robot_creation(recycling_robot_mdp: MDP):
             "high": {
                 "search": [("high", alpha, rsearch), ("low", "*", rsearch)],
                 "wait": [("high", "*", rwait)],
-        },
+            },
             "low": {
                 "search": [("low", beta, rsearch), ("high", "*", rempty)],
                 "wait": [("low", "*", rwait)],
                 "recharge": [("high", "*", 0.0)]
+            }
         }
-    }
 
-    mdp = MDP(states, actions, transitions)
+        mdp = MDP(states, actions, transitions)
 
-    # States and actions
-    assert set(mdp.states) == {"high", "low"}
-    assert set(mdp.actions) == {"search", "wait", "recharge"}
+        # States and actions
+        assert set(mdp.states) == {"high", "low"}
+        assert set(mdp.actions) == {"search", "wait", "recharge"}
 
-    # Terminal states
-    assert len(mdp.terminal_states) == 0
+        # Terminal states
+        assert len(mdp.terminal_states) == 0
 
-    # Admissible actions
-    for state, actions in mdp.admissible_actions.items():
-        for action in actions:
-            assert action in mdp.actions
+        # Admissible actions
+        for state, actions in mdp.admissible_actions.items():
+            for action in actions:
+                assert action in mdp.actions
 
-    # Transition probabilities sum to 1
-    for state in mdp.states:
-        for action in mdp.admissible_actions[state]:
-            probs = mdp.P(state, action)
-            assert math.isclose(probs.sum(), 1.0), f"P sum != 1 for {state}, {action}"
+        # Transition probabilities sum to 1
+        for state in mdp.states:
+            for action in mdp.admissible_actions[state]:
+                probs = mdp.P(state, action)
+                assert math.isclose(probs.sum(), 1.0), f"P sum != 1 for {state}, {action}"
 
-    # Rewards are numbers
-    for state in mdp.states:
-        for action in mdp.admissible_actions[state]:
-            rewards = mdp.R(state, action)
-            assert all(isinstance(r, float) for r in rewards), f"Rewards not floats for {state}, {action}"
+        # Rewards are numbers
+        for state in mdp.states:
+            for action in mdp.admissible_actions[state]:
+                rewards = mdp.R(state, action)
+                assert all(isinstance(r, float) for r in rewards), f"Rewards not floats for {state}, {action}"
